ba31a3de0040aa3ff52d2d36099ff0d38518590d
scdgr <36089115+scdgr@users.noreply.github.com>
2018-06-09 18:40:32 -0300

    Update ControleChat.java

--- src/main/java/br/com/pitagorasgurarapari/parcial2oo/ControleChat.java
+++ src/main/java/br/com/pitagorasgurarapari/parcial2oo/ControleChat.java
@@ -1,29 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.pitagorasgurarapari.parcial2oo;
 
 import com.pusher.client.PusherOptions;
 import com.pusher.client.channel.Channel;
+import com.pusher.client.channel.SubscriptionEventListener;
 import com.pusher.rest.Pusher;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- *
- * @author alunos
- */
 public class ControleChat {
     
     private Pusher pusher;
     private Channel channel;
     com.pusher.client.Pusher pusherCliente;
     
-    private static final String APP_ID ="//TODO COLOCAR ID DO SEU APP";
-    private static final String KEY = "//TODO COLOCAR A SUA CHAVE";
-    private static final String SECRET = "//TODO COLOCAR O SEU SEGREDO";
+    private static final String APP_ID ="535996";
+    private static final String KEY = "6aaab1066467567d4da5";
+    private static final String SECRET = "97c62a01600e7dbc535f";
     
     public void inicializaPusher(){
         pusher = new Pusher(APP_ID, KEY, SECRET);
@@ -36,20 +28,18 @@ public class ControleChat {
         channel = pusherCliente.subscribe("pitagoras-guarapari");
         //TODO CONECTAR O pusherClient para poder receber as mensagens
         
+        pusherCliente.connect();
     }
     
     public void enviarMenssagem(Usuario user, String mensagem){
         Map<String,String> conteudo = new HashMap<>();
         conteudo.put("usuario", user.getNome());
         conteudo.put("mensagem", mensagem);
+        pusher.trigger("pitagoras-guarapari", "imagem-acao", conteudo);
         //TODO FAZER O ENVIO DA MENSAGEM USANDO O OBJETO pusher
     }
 
     public Channel getChannel() {
         return channel;
-    }
-    
-    
-    
-    
+    }                
 }
